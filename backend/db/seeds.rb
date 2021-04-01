37c74a764618d2196103d20310a43c0751bcde15
nickd <nickdiaz58@gmail.com>
2021-04-01 10:46:59 -0400

    Created models and associations, added seed file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -6,4 +6,13 @@ Game.destroy_all
 u1 = User.create(name: "Kirk")
 u2 = User.create(name: "Spock")
 
+g1 = Game.create(completed: true, won: true)
+g2 = Game.create(completed: true, won: false)
+g3 = Game.create(completed: false, won: false)
+g4 = Game.create(completed: true, won: true)
+g5 = Game.create(completed: true, won: true)
+
+u1.games.push(g1, g2, g3)
+u2.games.push(g4, g5)
+
 puts "Seeding complete."
